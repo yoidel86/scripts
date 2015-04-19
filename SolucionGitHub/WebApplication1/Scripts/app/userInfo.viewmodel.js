22ef321f5656d59b03d433932b5bd6971a9ca29f
yoidel86 <yoidel86@gmail.com>
2015-04-19 10:25:19 -0400

    esto es un ejemplo de github local con visual studio

---
+++ SolucionGitHub/WebApplication1/Scripts/app/userInfo.viewmodel.js
@@ -0,0 +1,19 @@
+﻿function UserInfoViewModel(app, name, dataModel) {
+    var self = this;
+
+    // Datos
+    self.name = ko.observable(name);
+
+    // Operaciones
+    self.logOff = function () {
+        dataModel.logout().done(function () {
+            app.navigateToLoggedOff();
+        }).fail(function () {
+            app.errors.push("Error al cerrar sesión.");
+        });
+    };
+
+    self.manage = function () {
+        app.navigateToManage();
+    };
+}
